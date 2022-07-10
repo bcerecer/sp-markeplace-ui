789d476f816569971553237bce8e2401fe99365c
Blas Cerecer <blas@staratlas.com>
2022-07-10 15:55:27 -0700

    feat: add Martian walet provider connection

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,14 +2,30 @@ import { AppProps } from 'next/app';
 import '../styles/globals.css';
 import 'tailwindcss/tailwind.css';
 import AppNavbar from '../components/AppNavbar/AppNavbar';
+import { useEffect, useState } from 'react';
+
+declare global {
+  interface Window {
+    aptos: any;
+    martian: any;
+  }
+}
 
 const App = ({ Component, pageProps }: AppProps): JSX.Element => {
-  return (
-    <div>
-      <AppNavbar />
-      <Component {...pageProps} />
-    </div>
+  // Makes sure window is not undefined
+  const [app, setApp] = useState(<></>);
+  useEffect(
+    () =>
+      setApp(
+        <div>
+          <AppNavbar />
+          <Component {...pageProps} />
+        </div>
+      ),
+    []
   );
+
+  return app;
 };
 
 export default App;
--- src/views/Landing/LandingView.tsx
+++ src/views/Landing/LandingView.tsx
@@ -1,5 +1,34 @@
+import React from 'react';
+
 const LandingView = (): JSX.Element => {
-  return <div>Hello LandingView!</div>;
+  const [address, setAddress] = React.useState<string | null>(null);
+  const getMartianProvider = () => {
+    const isMartianWalletInstalled = window.martian;
+
+    if (isMartianWalletInstalled) {
+      // Disconnect
+      if (address) {
+        window.martian.disconnect();
+        setAddress(null);
+        return;
+      }
+
+      // Connect
+      return window.martian.connect((resp) => {
+        console.log('resp: ', resp);
+        setAddress(resp.address);
+        // 0xc4265dc8a5d90715f8a60bebf16688819427bca928a537ad35f798d4d1267716
+      });
+    }
+    window.open('https://www.martianwallet.xyz/', '_blank');
+  };
+
+  return (
+    <div>
+      <button onClick={() => getMartianProvider()}>Connect Martian wallet</button>
+      <div>{address}</div>
+    </div>
+  );
 };
 
 export default LandingView;

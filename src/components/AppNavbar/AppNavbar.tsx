670a72ff3a49a6ded57a509bd4a3c2af4d374717
Blas Cerecer <blas@staratlas.com>
2022-07-10 01:17:04 -0700

    feat: add AppNavbar

---
+++ src/components/AppNavbar/AppNavbar.tsx
@@ -0,0 +1,49 @@
+import { Navbar, Button, Typography } from '@material-tailwind/react';
+import router from 'next/router';
+
+export const AppNavbar = (): JSX.Element => {
+  return (
+    <Navbar className="mx-auto w-full">
+      <div className="container flex items-center justify-between text-blue-grey-900">
+        <Typography
+          as="a"
+          href="#"
+          variant="small"
+          className="mr-4 cursor-pointer py-1.5 font-normal"
+          onClick={() => {
+            router.push('/');
+          }}
+        >
+          Apart
+        </Typography>
+        <ul className="flex items-center gap-6">
+          <Typography
+            as="li"
+            variant="small"
+            className="p-1 cursor-pointer font-normal"
+            onClick={() => {
+              router.push('/collections');
+            }}
+          >
+            <a className="flex items-center">Collections</a>
+          </Typography>
+          <Typography
+            as="li"
+            variant="small"
+            className="p-1 cursor-pointer font-normal"
+            onClick={() => {
+              router.push('/profile');
+            }}
+          >
+            <a className="flex items-center">Profile</a>
+          </Typography>
+        </ul>
+        <Button variant="gradient" size="sm">
+          Connect Wallet
+        </Button>
+      </div>
+    </Navbar>
+  );
+};
+
+export default AppNavbar;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,15 @@
 import { AppProps } from 'next/app';
 import '../styles/globals.css';
 import 'tailwindcss/tailwind.css';
+import AppNavbar from '../components/AppNavbar/AppNavbar';
 
 const App = ({ Component, pageProps }: AppProps): JSX.Element => {
-  return <Component {...pageProps} />;
+  return (
+    <div>
+      <AppNavbar />
+      <Component {...pageProps} />
+    </div>
+  );
 };
 
 export default App;

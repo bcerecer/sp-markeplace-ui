957ecb0f14d9cfa73e2ad570db73a08d7e1137e1
Blas Cerecer <blas@staratlas.com>
2022-07-13 15:54:14 -0700

    feat: styled AppNavbar component

--- src/components/AppNavbar/AppNavbar.tsx
+++ src/components/AppNavbar/AppNavbar.tsx
@@ -4,44 +4,48 @@ import ConnectWalletButton from './components/ConnectWalletButton/ConnectWalletB
 
 export const AppNavbar = (): JSX.Element => {
   return (
-    <Navbar className="mx-auto w-full">
-      <div className="container flex items-center justify-between text-blue-grey-900">
-        <Typography as="a" variant="small" className="mr-4 py-1.5 font-normal">
-          Space Powder
-        </Typography>
-        <ul className="flex items-center gap-6">
-          <Typography
-            as="li"
-            variant="small"
-            className="p-1 cursor-pointer font-normal"
-            onClick={() => {
-              router.push('/');
-            }}
-          >
-            <a className="flex items-center">Marketplace</a>
+    <Navbar className="mx-auto w-full" fullWidth>
+      <div className="w-full flex items-center justify-between text-blue-grey-900">
+        <div>
+          <Typography as="a" variant="small" className="mr-4 py-1.5 font-normal">
+            Space Powder
           </Typography>
-          <Typography
-            as="li"
-            variant="small"
-            className="p-1 cursor-pointer font-normal"
-            onClick={() => {
-              router.push('/create');
-            }}
-          >
-            <a className="flex items-center">Create</a>
-          </Typography>
-          <Typography
-            as="li"
-            variant="small"
-            className="p-1 cursor-pointer font-normal"
-            onClick={() => {
-              router.push('/profile');
-            }}
-          >
-            <a className="flex items-center">Profile</a>
-          </Typography>
-        </ul>
-        <ConnectWalletButton />
+        </div>
+        <div>
+          <ul className="flex items-center gap-6">
+            <Typography
+              as="li"
+              variant="small"
+              className="p-1 cursor-pointer font-normal"
+              onClick={() => {
+                router.push('/');
+              }}
+            >
+              <a className="flex items-center">Marketplace</a>
+            </Typography>
+            <Typography
+              as="li"
+              variant="small"
+              className="p-1 cursor-pointer font-normal"
+              onClick={() => {
+                router.push('/create');
+              }}
+            >
+              <a className="flex items-center">Create</a>
+            </Typography>
+            <Typography
+              as="li"
+              variant="small"
+              className="p-1 cursor-pointer font-normal"
+              onClick={() => {
+                router.push('/profile');
+              }}
+            >
+              <a className="flex items-center">Profile</a>
+            </Typography>
+            <ConnectWalletButton />
+          </ul>
+        </div>
       </div>
     </Navbar>
   );

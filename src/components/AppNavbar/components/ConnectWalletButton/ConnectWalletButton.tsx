ec3f7698fd38b489d17be733549ee05f97bad31b
Blas Cerecer <blas@staratlas.com>
2022-07-13 16:19:47 -0700

    feat: style ConnectWalletButton component

--- src/components/AppNavbar/components/ConnectWalletButton/ConnectWalletButton.tsx
+++ src/components/AppNavbar/components/ConnectWalletButton/ConnectWalletButton.tsx
@@ -26,8 +26,36 @@ export const ConnectWalletButton = (): JSX.Element => {
   };
 
   return (
-    <Button variant="gradient" size="sm" onClick={() => getMartianProvider()}>
-      {wallet.address ? 'Disconnect Wallet' : 'Connect Wallet'}
+    <Button
+      variant="gradient"
+      size="sm"
+      onClick={() => getMartianProvider()}
+      className="flex items-center"
+    >
+      <div className="pr-[5px]">
+        <svg
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M3 7.5H18.5C19.8807 7.5 21 8.61929 21 10V17C21 18.3807 19.8807 19.5 18.5 19.5H5.5C4.11929 19.5 3 18.3807 3 17V7.5Z"
+            stroke="white"
+            stroke-width="1.25"
+            stroke-linejoin="round"
+          />
+          <path
+            d="M19 7.5V6.5C19 5.39543 18.1046 4.5 17 4.5H5C3.89543 4.5 3 5.39543 3 6.5V7.5"
+            stroke="white"
+            stroke-width="1.25"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          />
+        </svg>
+      </div>
+      {wallet.address ? 'Disconnect' : 'Connect'}
     </Button>
   );
 };

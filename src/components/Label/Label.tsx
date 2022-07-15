73d0b7b6d83c10651b7bb58667e2bcd63a56274c
Blas Cerecer <blas@staratlas.com>
2022-07-14 17:45:26 -0700

    feat: add Label component

---
+++ src/components/Label/Label.tsx
@@ -0,0 +1,16 @@
+import type { ComponentProps, FC, PropsWithChildren } from 'react';
+import classNames from 'classnames';
+
+export interface LabelProps
+  extends PropsWithChildren<Omit<ComponentProps<'label'>, 'className' | 'color'>> {
+  className: string;
+  disabled?: boolean;
+}
+
+export const Label: FC<LabelProps> = ({ children, className = '' }): JSX.Element => {
+  return (
+    <label className={classNames('font-medium dark:text-white', className)}>{children ?? ''}</label>
+  );
+};
+
+export default Label;

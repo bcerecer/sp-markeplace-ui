fb0961a07703a2e731a2fe773dc68f3ae72c6324
Blas Cerecer <blas@staratlas.com>
2022-07-15 10:20:00 -0700

    feat: migrate views to pages folder and remove views folder

--- src/pages/create.tsx
+++ src/pages/create.tsx
@@ -1,7 +1,16 @@
-import CreateView from '../views/Create/CreateView';
+import CreateForm from '../components/CreateForm/CreateForm';
+import { Label } from '../components/Label/Label';
 
 const CreatePage = (): JSX.Element => {
-  return <CreateView />;
+  return (
+    <div className="w-full flex flex-col items-center justify-center">
+      <Label className="text-6xl font-extrabold p-4 tracking-wide">CREATE</Label>
+      <Label className="text-xl font-bold p-7">
+        Submit your Aptos NFT collection in Space Powder!
+      </Label>
+      <CreateForm />
+    </div>
+  );
 };
 
 export default CreatePage;

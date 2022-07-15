f0594d9ec895ba0f0aefc3d2efae3f5283cbca08
Blas Cerecer <blas@staratlas.com>
2022-07-14 22:04:10 -0700

    feat: update ProfileView component

--- src/views/Profile/ProfileView.tsx
+++ src/views/Profile/ProfileView.tsx
@@ -1,17 +1,17 @@
+import Label from '../../components/Label/Label';
 import NftsGrid from '../../components/NftsGrid/NftsGrid';
 import { useGlobalState } from '../../utils/state';
 
 const ProfileView = (): JSX.Element => {
   const [wallet, _] = useGlobalState('wallet');
   return (
-    <div className="w-full flex-column items-center">
+    <div className="w-full flex flex-col items-center justify-center">
+      <Label className="text-6xl font-extrabold p-4 tracking-wide">PROFILE</Label>
+      <Label className="text-xl font-bold p-7">Browse your NFTs and list them to Marketplace</Label>
       {wallet.address ? (
-        <div>
-          <div className="container mx-auto">Connected address: {wallet.address}</div>
-          <NftsGrid></NftsGrid>
-        </div>
+        <NftsGrid />
       ) : (
-        <div className="w-full flex justify-center">Connect wallet</div>
+        <Label className="text-md w-full flex justify-center">Please connect your wallet</Label>
       )}
     </div>
   );

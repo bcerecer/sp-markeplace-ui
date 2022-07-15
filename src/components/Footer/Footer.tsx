e6242eba6f40a0970617583832fa1889c0dc37a9
Blas Cerecer <blas@staratlas.com>
2022-07-14 20:51:13 -0700

    feat: add redirection links to Footer component

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,5 +1,6 @@
 import { Footer as FlowFooter } from 'flowbite-react';
 import { BsGithub, BsTwitter } from 'react-icons/bs';
+import { HiMail } from 'react-icons/hi';
 
 const Footer = () => {
   return (
@@ -9,8 +10,9 @@ const Footer = () => {
         <div className="w-full sm:flex sm:items-center sm:justify-between">
           <FlowFooter.Copyright href="#" by=" Space Powder" year={2022} />
           <div className="mt-4 flex space-x-6 sm:mt-0 sm:justify-center">
-            <FlowFooter.Icon href="#" icon={BsTwitter} />
-            <FlowFooter.Icon href="#" icon={BsGithub} />
+            <FlowFooter.Icon href="https://twitter.com/spacepowdernft" icon={BsTwitter} />
+            <FlowFooter.Icon href="https://github.com/spacepowderlabs" icon={BsGithub} />
+            <FlowFooter.Icon href="mailto:spacepowderio@gmail.com" icon={HiMail} />
           </div>
         </div>
       </div>

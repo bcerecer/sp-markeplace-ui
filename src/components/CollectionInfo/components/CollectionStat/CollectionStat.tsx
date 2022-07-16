cc44765cc1bddc625e8f69a203cba71cde2cded3
Blas Cerecer <blas@staratlas.com>
2022-07-15 20:58:14 -0700

    feat: add CollectionStat component

---
+++ src/components/CollectionInfo/components/CollectionStat/CollectionStat.tsx
@@ -0,0 +1,18 @@
+import Label from '../../../../components/Label/Label';
+
+type CollectionStatProp = {
+  title: string;
+  value: string | number;
+};
+
+const CollectionStat = (props: CollectionStatProp) => {
+  const { title, value } = props;
+  return (
+    <div className="w-10/12 justify-self-end	h-10 dark:bg-blue-800 rounded-md p-2 flex items-center justify-between">
+      <Label className="w-1/2 pl-2 text-xs font-bold dark:text-gray-400">{title}</Label>
+      <Label className="w-1/2 pr-2 text-sm font-md flex justify-end overflow-hidden">{value}</Label>
+    </div>
+  );
+};
+
+export default CollectionStat;

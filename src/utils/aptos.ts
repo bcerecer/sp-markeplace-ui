24996b5f738974e034736372fc9b6fc76e2d2a0b
Blas Cerecer <blas@staratlas.com>
2022-07-18 16:18:44 -0700

    feat: add Martian clients to utils/aptos.ts

--- src/utils/aptos.ts
+++ src/utils/aptos.ts
@@ -1,17 +1,23 @@
-import { AptosAccount, AptosClient, FaucetClient } from 'aptos';
+import { AptosClient, FaucetClient, TokenClient } from 'aptos';
+import { WalletClient } from '@martiandao/aptos-web3-bip44.js';
 
-const NODE_URL = process.env.APTOS_NODE_URL || 'https://fullnode.devnet.aptoslabs.com';
-const FAUCET_URL = process.env.APTOS_FAUCET_URL || 'https://faucet.devnet.aptoslabs.com';
+export const NODE_URL = process.env.APTOS_NODE_URL || 'https://fullnode.devnet.aptoslabs.com';
+export const FAUCET_URL = process.env.APTOS_FAUCET_URL || 'https://faucet.devnet.aptoslabs.com';
+export const TOKEN_STORE = '0x1::Token::TokenStore';
 
 const getAptosClient = (): AptosClient => {
   return new AptosClient(NODE_URL);
 };
+export const aptosClient = getAptosClient();
 
 const getFaucetClient = (): FaucetClient => new FaucetClient(NODE_URL, FAUCET_URL);
-
-export const aptosClient = getAptosClient();
 export const aptosFaucetClient = getFaucetClient();
 
-export const fundAptosAccount = async (account: AptosAccount) => {
-  await aptosFaucetClient.fundAccount(account.address(), 5000);
-};
+const getTokenClient = (): TokenClient => new TokenClient(aptosClient);
+export const aptosTokenClient = getTokenClient();
+
+const getMartianWalletClient = () => new WalletClient(NODE_URL, FAUCET_URL);
+export const martianWalletClient = getMartianWalletClient();
+
+const getMartianTokenClient = () => new TokenClient(aptosClient);
+export const martianTokenClient = getMartianTokenClient();

52913b418824d62e94acb6c35f7432e44598ec03
Blas Cerecer <blas@staratlas.com>
2022-07-18 18:35:27 -0700

    feat: add SpacePowderClient to utils

--- src/utils/aptos.ts
+++ src/utils/aptos.ts
@@ -1,9 +1,12 @@
 import { AptosClient, FaucetClient, TokenClient } from 'aptos';
 import { WalletClient } from '@martiandao/aptos-web3-bip44.js';
+import { SpacePowderClient } from 'src/blockchain/space-powder/fixed-price-sale';
 
 export const NODE_URL = process.env.APTOS_NODE_URL || 'https://fullnode.devnet.aptoslabs.com';
 export const FAUCET_URL = process.env.APTOS_FAUCET_URL || 'https://faucet.devnet.aptoslabs.com';
 export const TOKEN_STORE = '0x1::Token::TokenStore';
+
+/**************** APTOS ****************/
 
 const getAptosClient = (): AptosClient => {
   return new AptosClient(NODE_URL);
@@ -16,8 +19,15 @@ export const aptosFaucetClient = getFaucetClient();
 const getTokenClient = (): TokenClient => new TokenClient(aptosClient);
 export const aptosTokenClient = getTokenClient();
 
+/**************** MARTIAN ****************/
+
 const getMartianWalletClient = () => new WalletClient(NODE_URL, FAUCET_URL);
 export const martianWalletClient = getMartianWalletClient();
 
 const getMartianTokenClient = () => new TokenClient(aptosClient);
 export const martianTokenClient = getMartianTokenClient();
+
+/**************** SPACE POWDER ****************/
+
+const getSpacePowderClient = () => new SpacePowderClient(aptosClient);
+export const spacePowderClient = getSpacePowderClient();

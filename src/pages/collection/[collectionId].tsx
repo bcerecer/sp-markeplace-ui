a94b43fc0a9465154f50e7f24ada2a88b3cce999
Blas Cerecer <blas@staratlas.com>
2022-07-20 19:16:51 -0700

    feat: dynamically get collection data from supabase

--- src/pages/collection/[collectionId].tsx
+++ src/pages/collection/[collectionId].tsx
@@ -1,45 +1,108 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Grid from '@components/Grid/Grid';
-import CollectionInfo from '@components/CollectionInfo/CollectionInfo';
-import { useRouter } from 'next/router';
+import CollectionInfo, { CollectionInfoProps } from '@components/CollectionInfo/CollectionInfo';
 import { TokenCardProps } from '@components/Grid/components/TokenCard/TokenCard';
-import { collectionsData } from 'src/data/collectionsData';
+import { supabaseClient } from 'src/utils/supabase';
+import { useRouter } from 'next/router';
+import { Spinner } from 'flowbite-react';
 
-// TODO: remove this once we go to database solution
-export const getServerSideProps = async () => {
-  return {
-    props: {
-      collections: collectionsData,
-    },
-  };
+const getSupaCollection = async (collectionPath: string) => {
+  const { data: collectionData } = await supabaseClient
+    .from('collections')
+    .select('*')
+    .eq('path', collectionPath);
+  return collectionData;
 };
 
-const fetchedStats = {
-  owners: '1',
-  floor: '1,000,000',
-  listed: '10',
-  totalSupply: '27',
+const getSupaTokens = async (collectionName: string) => {
+  const { data: tokensData } = await supabaseClient
+    .from('tokens')
+    .select('*')
+    .eq('collection_name', collectionName);
+  return tokensData;
 };
 
-const CollectionPage = ({ collections }: { collections: any }): JSX.Element => {
+type Collection = {
+  name: string;
+  tokensCreated: number;
+  likes: number;
+  ownerAddress: string;
+  description: string;
+  path: string;
+};
+
+const CollectionPage = (): JSX.Element => {
   const router = useRouter();
+  const [collection, setCollection] = useState<Collection>();
+  const [collectionTokens, setCollectionTokens] = useState<TokenCardProps[]>();
+  const [collectionInfoData, setCollectionInfoData] = useState<CollectionInfoProps>();
 
-  const collection = collections?.find((collection: any) => collection.path === router.asPath);
-  const collectionInfoData = {
-    creatorAddress: collection.address,
-    collectionName: collection.name,
-    collectionDescription: collection.description,
-    collectionImgSrc: collection.imgSrc,
-    stats: fetchedStats,
-  };
+  // Only get it the first time
+  if (!collection) {
+    getSupaCollection(router.asPath.split('/').pop() as string).then((supaCollection: any) => {
+      // Make sure that collection exists
+      if (supaCollection.length > 0) {
+        setCollection({
+          name: supaCollection[0].name,
+          tokensCreated: supaCollection[0].tokens_created,
+          likes: supaCollection[0].likes,
+          ownerAddress: supaCollection[0].owner_address,
+          description: supaCollection[0].description,
+          path: supaCollection[0].path,
+        });
 
-  const gridTokensData: TokenCardProps[] = collection.tokens;
+        setCollectionInfoData({
+          creatorAddress: supaCollection[0].owner_address,
+          collectionName: supaCollection[0].name,
+          collectionDescription: supaCollection[0].description,
+          collectionImgSrc: supaCollection[0].img_url,
+          stats: {
+            owners: '1',
+            floor: '1,000,000',
+            listed: '10',
+            totalSupply: '27',
+          },
+        });
+      }
+    });
+  }
+
+  // Collection is fetched
+  if (collection && !collectionTokens) {
+    getSupaTokens(collection.name).then((supaTokens: any) => {
+      // Make sure tokens for collection exists
+      if (supaTokens.length > 0) {
+        setCollectionTokens(
+          supaTokens.map((supaToken: any) => {
+            return {
+              variant: supaToken.listed ? 'listed' : 'unlisted',
+              imgSrc: supaToken.img_url,
+              collectionOwnerAddress: supaToken.collection_owner_address,
+              collectionName: supaToken.collection_name,
+              tokenName: supaToken.name,
+              sellerAddress: supaToken.seller_address,
+              price: supaToken.price,
+            } as TokenCardProps;
+          })
+        );
+      }
+    });
+  }
 
   return (
     <div className="w-full flex flex-col">
-      <CollectionInfo {...collectionInfoData} />
+      {console.log('JSX rendered')}
+      {collectionInfoData ? (
+        <CollectionInfo {...collectionInfoData} />
+      ) : (
+        <Spinner aria-label="Spinner" size="xl" />
+      )}
       <div className="p-9">
-        <Grid items={gridTokensData} />
+        {collectionTokens ? (
+          <Grid items={collectionTokens} />
+        ) : (
+          <Spinner aria-label="Spinner" size="xl" />
+        )}
       </div>
     </div>
   );
